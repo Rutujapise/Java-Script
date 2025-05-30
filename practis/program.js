18237d0de32a0647d7d96d933d297f7688840f35
Rutuja <rutujapise28@gmail.com>
2025-05-30 21:40:18 +0530

    Operators and Expressions

---
+++ practis/program.js
@@ -0,0 +1,51 @@
+ //assignment 1
+num1= 10;
+num2 =30;
+console.log('Addition',num1+num2);
+console.log('Substraction',num1-num2);
+console.log('multiplication',num1*num2)
+console.log('division',num1/num2)
+console.log('modulas',num1%num2)
+// second assignment comparision oprators
+ a=40;
+let b= 67
+console.log("First no grater than second number",a>b)
+console.log("First no less than second number",a < b)
+console.log("First no and  second number is equal",a === b)
+console.log("First no is not equal than second number",a!==b)
+
+//assignment 3 Combine logical opraters.
+let isAdult =true;
+let hasLicense =false;
+console.log(isAdult && hasLicense);
+console.log(isAdult || hasLicense);
+console.log(!isAdult);
+// Assignment 4 demonstrate Operator precedence
+let result = 12+5*4-8/9;
+console.log(result);
+let boadmass1 =10+3*5;
+console.log(boadmass1)
+let boadmass2 =(10+3)*5;
+console.log(boadmass2)
+boadmass3 = 20/2+3;
+console.log(boadmass3);
+boadmass4 = 20/(2+3);
+console.log(boadmass4)
+
+// Assignment 5 using assignment Operators
+//let x =10;
+//x +=5;
+//console.log(x);
+//x*=2;
+//console.log(x);
+// assignment 1 for user input 
+// n1 = prompt("Enter  the first Number");
+// console.log(n1);
+// n2 = prompt("Enter  the second number Number");
+// console.log(n2);
+// console.log("Addition",n1+n2);
+// console.log("Substraction",n1-n2);
+// console.log("Multiplication",n1*n2);
+// console.log("Division",n1/n2);
+// console.log("Modulus",n1%n2);
+
